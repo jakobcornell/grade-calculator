274f93c3edd973904814a4bfbff1b9bcdda0e5f8
Jakob Cornell <jakobcornell@gmail.com>
2016-10-17 23:12:06 -0500

    Store weights and point values as floats

--- parsing.py
+++ parsing.py
@@ -8,16 +8,16 @@ def parse_course(infile):
 	)
 	for cat in crs['categories']:
 		category = Category(
-			weight = cat['weight'],
+			weight = float(cat['weight']),
 			name = cat['name'] if 'name' in cat else None,
 			weighting = cat['weighting'] if 'weighting' in cat else None
 		)
 		for assn in cat['assignments']:
 			assignment = Assignment(
-				earned = assn['earned'],
-				possible = assn['possible'],
+				earned = float(assn['earned']),
+				possible = float(assn['possible']),
 				name = assn['name'] if 'name' in assn else None,
-				weight = assn['weight'] if 'weight' in assn else 1
+				weight = float(assn['weight']) if 'weight' in assn else 1.0
 			)
 			category.assignments.append(assignment)
 		course.categories.append(category)
