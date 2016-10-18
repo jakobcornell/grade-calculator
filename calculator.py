f7d7cbad4d8349619e4fbc100b80a7348f5f07a9
Jakob Cornell <jakobcornell@gmail.com>
2016-10-17 23:35:00 -0500

    Handle unscorable course elegantly

--- calculator.py
+++ calculator.py
@@ -2,5 +2,10 @@ import sys
 import parsing
 
 course = parsing.parse_course(sys.stdin)
+score = course.score()
 
-print(course.score())
+if score is None:
+	print("This course cannot be scored. (Does it have assignments?)", file = sys.stderr)
+	sys.exit(1)
+else:
+	print(course.score())
